e941df68af930139a4306a1db295bf899b30c610
“kartik <kaxxtric001@gmail.com>
2025-07-17 12:07:22 +0530

    Initial commit: react-practice with Vite

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+function App() {
+
+  return <div>
+      <h1>
+        <b>
+          BASIC COUNTER
+        </b>
+
+      </h1>
+    <Counter></Counter>
+   
+  </div>
+}
+
+function Counter(){//component
+
+  const [count,setCount]=useState(0);//count is a state variable whose value get re rendered
+
+  function increaseCount(){
+    setCount(count+1);
+  }
+
+  function decreaseCount(){
+    setCount(count-1);
+  }
+  return <div>
+    <h1 id="text">{count}</h1>
+    <button onClick={increaseCount}>Increase count</button>
+    <button onClick={decreaseCount}>Decrease count</button>
+  </div>
+}
+export default App
