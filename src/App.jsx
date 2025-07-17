2d20614883d4f55ccec6f1455080375e0e0bad13
“kartik <kaxxtric001@gmail.com>
2025-07-17 14:00:29 +0530

    Slight functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,15 @@ function Counter(){//component
   function decreaseCount(){
     setCount(count-1);
   }
+
+  function resetCount(){
+    setCount(0);
+  }
   return <div>
     <h1 id="text">{count}</h1>
     <button onClick={increaseCount}>Increase count</button>
     <button onClick={decreaseCount}>Decrease count</button>
+    <button onClick={resetCount}>Reset count</button>
   </div>
 }
 export default App
