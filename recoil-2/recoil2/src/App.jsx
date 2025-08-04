1dfb1ef561bcf8ac9150af9ae0919eafef6e9f7e
kartik <kaxxtric001@gmail.com>
2025-08-04 17:39:14 +0530

    Selectors and recoil example1

---
+++ recoil-2/recoil2/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom, evenSelector } from './store/atoms/counter'
+
+function App() {
+
+
+  return <div>
+    <RecoilRoot>
+      <Buttons />
+      <Counter />
+      <IsEven />
+    </RecoilRoot>
+    
+  </div>
+}
+
+function Buttons(){
+
+  const setCount=useSetRecoilState(counterAtom);
+
+  function increase(){
+    setCount(c=>c+2)
+  }
+
+  function decrease(){
+    setCount(c=>c-1)
+  }
+  return <div>
+    <button onClick={increase}>Increase</button>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+function Counter(){
+  const count=useRecoilValue(counterAtom);
+
+  return <div>
+    {count}
+  </div>
+}
+
+
+function IsEven(){
+  const even=useRecoilValue(evenSelector);
+
+  return <div>
+    {even ? "Even":"Odd"}
+  </div>
+}
+
+export default App
---
+++ recoil-2/recoil2/src/store/atoms/counter.js
@@ -0,0 +1,17 @@
+import {atom, selector} from "recoil";
+
+export const counterAtom=atom({
+    default:0,
+    key:"counter"
+})
+
+//selector derived state
+
+export const evenSelector=selector({
+    key:"isEvenSelector",
+    get:function({get}){
+        const currentCount=get(counterAtom);
+        const isEven=(currentCount%2==0);
+        return isEven;
+    }
+})
